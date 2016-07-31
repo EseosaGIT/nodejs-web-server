bc80b9f75d021b09780e047b850a16e489fd9d36
Ehioghae Eseosa <eseosaehioghae@gmail.com>
2016-07-31 21:13:06 +0100

    Added About Page and did some styling

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -6,4 +6,8 @@ module.exports = function (app, middleware) {
     app.get('/contact', middleware.requireAuthentication, function(req, res){
         res.send('<html><head><title>Node app - contact</title><link rel="stylesheet" type="text/css" href="../assets/style.css"></head><body><h1>Node app here</h1><p>Contact Page!!!</p></body></html>');
     });
+
+    app.get('/about', middleware.requireAuthentication, function(req, res){
+        res.send('<html><head><title>Node app - about</title><link rel="stylesheet" type="text/css" href="../assets/style.css"></head><body><h1>Node app here</h1><h2>Hello to my first Node app in Heroku</h2><p>ABout Page!!!</p></body></html>');
+    });
 }
